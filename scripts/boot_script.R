fb1a09111aef1db4d1cd9a758a6a432a23f6e32d
Travis Canida <tacanida@gmail.com>
2017-12-18 17:01:06 -0500

    added primary boot loop

--- scripts/boot_script.R
+++ scripts/boot_script.R
@@ -1,2 +1,25 @@
 #Bootstrap confidence bounds
 library(openEBGM) #package containing functions and data
+
+theta_init <- data.frame(
+  alpha1 = c(0.2, 0.1),
+  beta1  = c(0.1, 0.1),
+  alpha2 = c(2,   10),
+  beta2  = c(4,   10),
+  p      = c(1/3, 0.2)
+)
+data(caers)
+B <- 2000
+for(i in 1:B) {
+    resamp <- caers[sample.int(n = nrow(caers), replace = TRUE),]
+    proc <- processRaw(resamp)
+    squash <- squashData(proc)
+    hypers <- try(autoHyper(data = squash, theta_init = theta_init, 
+                            squashed = TRUE, zeroes = FALSE, N_star = 1)$estimates,
+                  silent = TRUE)
+    a1 <- hypers[1]
+    b1 <- hypers[2]
+    a2 <- hypers[3]
+    b2 <- hypers[4]
+    p <- hypers[5]
+}
